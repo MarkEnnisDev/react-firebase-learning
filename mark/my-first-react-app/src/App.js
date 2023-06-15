b13fd33829379e5a775f1e6d9136f30445ed76c5
MarkEnnisDev <MarkEnnisDev@gmail.com>
2023-06-15 16:09:33 -0500

    create new repo

--- mark/my-first-react-app/src/App.js
+++ mark/my-first-react-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title'
+import Modal from './components/Modal';
 function App() {
   const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
@@ -8,7 +9,7 @@ function App() {
     {title: "bowser's live stream", id: 2},
     {title: "race on moo moo farm", id: 3},
   ])
-
+  const subtitle = "Hello I'm a subtitle"
   const handleClick = (id) => {  
     setEvents((prevEvents) => {
       return prevEvents.filter((event) => {
@@ -20,16 +21,19 @@ function App() {
   
   return (
     <div className="App">
-      <Title />
+      
+      <Title title="NET NINJA EVENTS" subtitle={subtitle}/>
+      <Title title="THIS IS A TITEL" subtitle="nooo"/>
       <div>
         <button onClick={() => setShowEvents(!showEvents)}>{!showEvents ? "show" : "hide"} events</button>
       </div>
           {showEvents && events.map((event, index) => (
-            <div key={event.id}>
+            <React.Fragment key={event.id}>
               <h2>{index} - {event.title}</h2>
               <button onClick={() => handleClick(event.id) }>delete event</button>
-            </div>
+            </React.Fragment>
           ))}
+          <Modal />
     </div>
   );
 }
---
+++ mark/my-first-react-app/src/components/Modal.js
@@ -0,0 +1,10 @@
+export default function Modal() {
+  return (
+    <div className="modal-backdrop">
+        <div className="modal">
+            <h2>10% Off Coupon Code!!</h2>
+            <p>Use the code SHINOBU10 at the checkout.</p>
+        </div>
+    </div>
+  )
+}
