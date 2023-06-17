0ff525664fdd1d1930fa63473393be2780f74073
MarkEnnisDev <MarkEnnisDev@gmail.com>
2023-06-17 16:03:33 -0500

    basic memory game my challenge complete

---
+++ mark/memory-game/src/App.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import SingleCard from './components/SingleCard'
+
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" },
+]
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+     .sort(() => Math.random() - 0.5)
+     .map((card) => ({ ...card, id: Math.random()}))
+
+     setCards(shuffledCards)
+
+     
+  }
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    const checkMatch = async () => {
+      if (!choiceOne || !choiceTwo) {
+        return
+      }
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("match")
+      } else {
+        console.log("not a match")
+        
+      }
+      resetTurn()
+    }
+    
+    checkMatch()
+  }, [choiceOne, choiceTwo])
+  
+  // resetTurn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard 
+          key={card.id} 
+          card={card} 
+          handleChoice={handleChoice}
+          />
+
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App
