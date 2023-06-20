d80243a7999412290fd434bc6cd17d042a8061eb
MarkEnnisDev <MarkEnnisDev@gmail.com>
2023-06-20 14:45:13 -0500

    Finalized non-firebase cooking site

--- cooking-ninja/src/components/Navbar.js
+++ cooking-ninja/src/components/Navbar.js
@@ -1,11 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import { useTheme } from '../hooks/useTheme';
+
 // styles
 import './Navbar.css'
 // components
 import Searchbar from './Searchbar';
+
+
+
 export default function Navbar() {
+  const { color } = useTheme()
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
         <nav>
             <NavLink to="/" className="brand">
                 <h1>Cooking Ninja</h1>
--- cooking-ninja/src/components/RecipeList.js
+++ cooking-ninja/src/components/RecipeList.js
@@ -1,16 +1,17 @@
 
+import { useTheme } from '../hooks/useTheme'
 import './RecipeList.css'
 // styles
 import { Link } from 'react-router-dom'
 export default function RecipeList({ recipes }) {
-  
+  const { mode } = useTheme()
   if (recipes.length === 0) {
     return <div className='error'>No recipes to load...</div>
   }
   return (
     <div className='recipe-list'>
         {recipes.map(recipe => (
-        <div className='card' key={recipe.id}>
+        <div className={`card ${mode}`} key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p>{recipe.cookingTime} to make</p>
             <div>{recipe.method.substring(0,100)}...</div>
