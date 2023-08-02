a7b55be0fcb0107aad102becbac3e5d8504e236d
MarkEnnisDev <MarkEnnisDev@gmail.com>
2023-08-02 13:46:46 -0500

    Basic Log in functionality

--- dojo-site/src/App.js
+++ dojo-site/src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Project from './pages/project/Project';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <Sidebar/>
+        <div className='container'>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/create' element={<Create/>}/>
+            <Route path='/projects/:id' element={<Project/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/signup' element={<Signup/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
