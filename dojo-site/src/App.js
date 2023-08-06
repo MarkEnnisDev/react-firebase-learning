51b91c9c7d94768e9ca41b550820ec24346aba88
MarkEnnisDev <MarkEnnisDev@gmail.com>
2023-08-06 15:38:46 -0500

    final push

--- dojo-site/src/App.js
+++ dojo-site/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 import './App.css'
 import Dashboard from './pages/dashboard/Dashboard';
 import Create from './pages/create/Create';
@@ -8,23 +9,29 @@ import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 
+import OnlineUsers from './components/OnlineUsers';
+
 
 function App() {
+  const { user, authIsReady } = useAuthContext()
   return (
     <div className="App">
-      <BrowserRouter>
-      <Sidebar/>
-        <div className='container'>
-          <Navbar/>
-          <Routes>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='/create' element={<Create/>}/>
-            <Route path='/projects/:id' element={<Project/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/signup' element={<Signup/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+        <Sidebar/>
+          <div className='container'>
+            <Navbar/>
+            <Routes>
+              <Route path='/' element={user ? <Dashboard/> : <Navigate to="/login"/>}/>
+              <Route path='/create' element={user ? <Create/> : <Navigate to="/login"/>}/>
+              <Route path='/projects/:id' element={user ? <Project/> : <Navigate to="/login"/>}/>
+              <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
+              <Route path='/signup' element={!user ? <Signup/> : <Navigate to="/"/>}/>
+            </Routes>
+          </div>
+          {user && <OnlineUsers/>}
+        </BrowserRouter>
+      )}
     </div>
   );
 }
